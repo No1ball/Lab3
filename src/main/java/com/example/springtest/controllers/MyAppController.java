5063debaf5f71960091782f17b8265f2941f1b7e
No1ball <noonepoint@gmail.com>
2022-04-22 03:46:00 +0300

    Add classes.

--- src/main/java/com/example/springtest/controllers/MyAppController.java
+++ src/main/java/com/example/springtest/controllers/MyAppController.java
@@ -1,4 +1,4 @@
-package com.example.springtest;
+package com.example.springtest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
